da23fb591b7b44c8fd6d7313c03ae2969c4c5685
Tamashiro90 <leandro.tamashiro90@gmail.com>
2024-08-29 21:28:17 -0300

    Add files via upload

---
+++ Aula_01/script.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+alert("Hello, World");
+//let preco = prompt("Digite o preço");
+//document.write(preco);
+
+let nome = prompt("Digite o seu nome")
+document.write("Olá ", nome, ". Seja bem-vindx ao site")
